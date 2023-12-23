f65a6444494437a107f6b95eb928928144d8ea20
SeungWon Seo <ttd8591@gmail.com>
2023-12-23 19:06:24 +0900

    [Init]

--- codes/llm/chat_llm.py
+++ codes/llm/chat_llm.py
@@ -4,8 +4,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 # CUDA 사용 가능 여부 확인
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/polyglot-ko-12.8b")
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/polyglot-ko-12.8b").to(device)
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/polyglot-ko-5.8b")
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/polyglot-ko-5.8b").to(device)
 
 # Example text prompt
 prompt = "{prompt: 너는 지금부터 인터넷 방송의 시청자의 역할이야. 이런 역할에 맞게 방송인의 이야기에 반응해줘.} {방송인: 오늘 날씨 좋다.} {시청자:}"
