7916193740dace975912e37170064a62f67c6f20
SeungWon Seo <ttd8591@gmail.com>
2023-12-23 19:29:02 +0900

    [ADD] add args

--- codes/llm/chat_llm.py
+++ codes/llm/chat_llm.py
@@ -1,5 +1,13 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import argparse
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Generate text from a prompt using a language model.")
+parser.add_argument('prompt', type=str, help='The prompt to feed to the language model')
+
+# Parse the arguments
+args = parser.parse_args()
 
 # CUDA 사용 가능 여부 확인
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -7,8 +15,8 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/polyglot-ko-5.8b")
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/polyglot-ko-5.8b").to(device)
 
-# Example text prompt
-prompt = "{prompt: 너는 지금부터 인터넷 방송의 시청자의 역할이야. 이런 역할에 맞게 방송인의 이야기에 반응해줘.} {방송인: 오늘 날씨 좋다.} {시청자:}"
+# Use the provided prompt
+prompt = args.prompt
 
 # Encode the prompt into tokens and convert to a PyTorch tensor
 input_ids = tokenizer.encode(prompt, return_tensors='pt').to(device)
